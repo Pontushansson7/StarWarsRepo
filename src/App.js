5a9903a39eb633f224b380766f1e6bed57e7b21a
47177677 <47177677+Pontushansson7@users.noreply.github.com>
2019-12-21 19:58:51 +0100

    get respons into array next

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import Characters from './components/characters';
 import './App.css';
 
 function App() {
   return (
     <Router basename="/starwarsapp/">
       <div className="App">
-      <NavLink to="/characters" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Home</NavLink>
+        <div className="App__Form">
+          
+          <div className="FormTitle">
+              <NavLink to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link" >Home</NavLink>
+              <NavLink to="/characters" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Characters</NavLink>
+          </div>
+
+            <Route exact path="/characters" component={Characters}>
+            </Route>
+            
+          </div>
       </div>
     </Router>
   );
